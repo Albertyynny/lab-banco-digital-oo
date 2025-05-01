efdc0d4c4c07ea47c73cb29cf64ee25914128f9b
Albertyynny <51037790+Albertyynny@users.noreply.github.com>
2025-05-01 10:10:32 -0300

    Implementa cheque especial

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -1,14 +1,45 @@
-
 public class ContaCorrente extends Conta {
+    
+    private double limiteChequeEspecial;
 
-	public ContaCorrente(Cliente cliente) {
-		super(cliente);
-	}
+    public ContaCorrente(Cliente cliente) {
+        super(cliente);
+        this.limiteChequeEspecial = 1000.0; 
+    }
+    
+    public ContaCorrente(Cliente cliente, double limiteChequeEspecial) {
+        super(cliente);
+        this.limiteChequeEspecial = limiteChequeEspecial;
+    }
 
-	@Override
-	public void imprimirExtrato() {
-		System.out.println("=== Extrato Conta Corrente ===");
-		super.imprimirInfosComuns();
-	}
-	
+    @Override
+    public void sacar(double valor) {
+        
+        if (valor <= (saldo + limiteChequeEspecial)) {
+            saldo -= valor;
+        } else {
+            System.out.println("Saldo insuficiente. Seu limite disponível é: " + 
+                String.format("%.2f", (saldo + limiteChequeEspecial)));
+        }
+    }
+
+    public double getLimiteChequeEspecial() {
+        return limiteChequeEspecial;
+    }
+
+    public void setLimiteChequeEspecial(double limiteChequeEspecial) {
+        this.limiteChequeEspecial = limiteChequeEspecial;
+    }
+    
+    public double getLimiteDisponivel() {
+        return saldo + limiteChequeEspecial;
+    }
+
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("=== Extrato Conta Corrente ===");
+        super.imprimirInfosComuns();
+        System.out.println(String.format("Limite de Cheque Especial: %.2f", this.limiteChequeEspecial));
+        System.out.println(String.format("Limite Disponível para Saque: %.2f", (this.saldo + this.limiteChequeEspecial)));
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,26 @@
-
 public class Main {
 
-	public static void main(String[] args) {
-		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
-		
-		Conta cc = new ContaCorrente(venilton);
-		Conta poupanca = new ContaPoupanca(venilton);
+    public static void main(String[] args) {
+        Cliente albertiny = new Cliente();
+        albertiny.setNome("Albertiny Rodrigues");
+        
+        ContaCorrente cc = new ContaCorrente(albertiny, 2000.0); 
+        ContaPoupanca poupanca = new ContaPoupanca(albertiny);
 
-		cc.depositar(100);
-		cc.transferir(100, poupanca);
-		
-		cc.imprimirExtrato();
-		poupanca.imprimirExtrato();
-	}
-
+        cc.depositar(100);
+        cc.imprimirExtrato();
+        
+        System.out.println("\n=== Teste de Cheque Especial ===");
+        System.out.println("Sacando R$ 500,00 (saldo de R$ 100,00)");
+        cc.sacar(500);
+        cc.imprimirExtrato();
+        
+        System.out.println("\n=== Teste de Transferência usando Cheque Especial ===");
+        System.out.println("Transferindo R$ 1000,00 para Poupança (saldo negativo)");
+        cc.transferir(1000, poupanca);
+        
+        System.out.println("\n=== Extratos Finais ===");
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+    }
 }
